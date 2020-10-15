42b3d1afff0f802b2a54b891da7e772dffdd27dc
Roshan chaudhary <roshan.chaudhary@tr15.successivetech.com>
2020-10-15 18:02:45 +0530

    docs: add git file

---
+++ extra/pattern/diamond.js
@@ -0,0 +1,23 @@
+function diamond(n)
+{
+    // using for space
+    let s= "";
+    //using for row
+    let j = n;
+    for (let i=1; i<=n ;i++)
+    {
+        s=" ".repeat(j);
+        console.log(s,'* '.repeat(i));
+        s="";
+        j--;
+    }
+    for(let i=n;i>=1;i--)
+    {
+        s=" ".repeat(j);
+        console.log(s,' *'.repeat(i));
+        s="";
+        j++;
+    }
+
+}
+diamond(5)
