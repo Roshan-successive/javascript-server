4e55e463172da22d8fefb8e27298d4bb8c7d5487
Roshan chaudhary <roshan.chaudhary@tr15.successivetech.com>
2020-11-04 17:50:47 +0530

    feat:updated file

--- extraTS/pattern/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.equitriangle = exports.diamond = void 0;
-var diamond_1 = require("./diamond");
-exports.diamond = diamond_1["default"];
-var equilateral_1 = require("./equilateral");
-exports.equitriangle = equilateral_1["default"];
